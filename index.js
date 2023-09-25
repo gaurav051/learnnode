9a9a227e8c1618ab0e6b7256f32ce38e93978a8f
gauravghale@gmail.com <gauravghale@gmail.com>
2023-09-25 22:22:47 +0545

    first

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+// const app = require("express")()
+
+const express =  require("express")
+const app = express()
+
+
+
+app.get('/',(req,res)=>{
+    // res.send("Hello")
+    res.json({message:"i am home page"})
+})
+app.get('/contact',(req,res)=>{
+    res.send("Contact")
+})
+app.get('/about',(req,res)=>{
+    res.send("About")
+})
+
+app.listen(3000,(req,res)=>{
+    console.log("Nodejs has started at port 3000")
+})
